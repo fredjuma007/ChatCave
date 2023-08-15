297aac33f57dc9eb9f6cdfafee2522bea5122ec1
fredjuma007 <fredjuma007>
2023-08-15 05:02:19 +0200

    the UI

--- ChatCave-frontend/src/ChatsPage.jsx
+++ ChatCave-frontend/src/ChatsPage.jsx
@@ -1,5 +1,21 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import {MultiChatSocket,MultiChatWindow,useMultiChatLogic} from 'react-chat-engine-advanced'
+/* Explaination of the imports respectively;
+ 1.user connect to all there chats
+ 2.the UI for the user to type etc
+ 3.makes the chatapp work
+*/
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '654c744a-3608-4ebc-945e-fbe483965a09',
+         props.user.username, 
+         props.user.secret
+    );
+    return(
+         <div style={{ height: '100vh' }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{ height: '100%' }} /> 
+        </div>
+    )
 }
 
 export default ChatsPage;
