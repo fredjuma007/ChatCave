f3dcc6b061876fc6041903b48a4a8b97cd78d45a
fredjuma007 <fredjuma007>
2023-08-15 03:54:24 +0200

    removing comments

--- ChatCave-frontend/src/AuthPage.jsx
+++ ChatCave-frontend/src/AuthPage.jsx
@@ -7,7 +7,7 @@ const AuthPage = (props) => {
   
     return (
       <div className="background">
-        <form onSubmit={onSubmit} className="form-card"> //html form element that takes in a username
+        <form onSubmit={onSubmit} className="form-card">
           <div className="form-title">Welcome 👋</div>
   
           <div className="form-subtitle">Set a username to get started</div>
@@ -15,7 +15,7 @@ const AuthPage = (props) => {
           <div className="auth">
             <div className="auth-label">Username</div>
             <input className="auth-input" name="username" />
-            <button className="auth-button" type="submit"> /*button that submits the username*/
+            <button className="auth-button" type="submit">
               Enter
             </button>
           </div>
