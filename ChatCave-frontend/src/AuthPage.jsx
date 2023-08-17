489a163ba1ef145cacf36a5e1c3999901e9a4506
fredjuma007 <fredjuma007>
2023-08-17 03:53:00 +0200

    opt

--- ChatCave-frontend/src/AuthPage.jsx
+++ ChatCave-frontend/src/AuthPage.jsx
@@ -15,7 +15,7 @@ const AuthPage = (props) => {
     return (
       <div className="background">
         <form onSubmit={onSubmit} className="form-card">
-          <div className="form-title">Welcome 👋</div>
+          <div className="form-title">ChatCave Welcome👋</div>
   
           <div className="form-subtitle">Set a username to get started</div>
   
